ab15f4c19c9bd3e5f1a325bbe49fbad1e6d4b61b
GetSkooled <hwixley1@gmail.com>
2021-01-27 15:18:25 +0000

    Colour formatting for tabs and segues added for ControlSection buttons

---
+++ DrPhil Controller/DrPhil Controller/System/ControlViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ControlViewController.swift
+//  DrPhil Controller
+//
+//  Created by GetSkooled on 27/01/2021.
+//  Copyright © 2021 Klean. All rights reserved.
+//
+
+import UIKit
+
+class ControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func unwindToControlSegue(_ segue: UIStoryboardSegue) {
+    }
+
+}
