2461cbfa446347418ec03f44575b1ea39e4c60bd
makspll <makspl17@gmail.com>
2021-01-29 03:37:22 +0000

    Improved API, added more devices

--- ros-workspace/src/dr-phil/src/webots_bridge/devices/Motor.py
+++ ros-workspace/src/dr-phil/src/webots_bridge/devices/Motor.py
@@ -9,15 +9,26 @@ class RotationalMotor(Device):
         self.set_position_srv = rospy.ServiceProxy(self.get_service_name("set_position"),set_float)
         self.set_velocity_srv = rospy.ServiceProxy(self.get_service_name("set_velocity"),set_float)
 
+        self.vel = 0
+        self.pos = 0
+
+        self.last_vel = 0
+        self.last_pos = 0
 
     def set_velocity(self,vel):
-        self.set_velocity_srv(vel)
+        self.vel = vel
 
     def set_position(self,pos):
-        self.set_position_srv(pos)
+        self.pos = pos 
 
     def update_device(self,dt):
-        pass
+        if self.last_vel != self.vel:
+            self.set_velocity_srv(self.vel)
+            self.last_vel = self.vel
+
+        if self.last_pos != self.pos:
+            self.set_position_srv(self.pos)
+            self.last_pos = self.pos
 
     def setup_device(self,frequency=15):
         pass
---
+++ ros-workspace/src/dr-phil/src/webots_bridge/robots/waffle.py
@@ -0,0 +1,56 @@
+
+from devices.Lidar import Lidar 
+from devices.Motor import RotationalMotor
+from geometry_msgs.msg import Twist
+import rospy
+
+class Waffle:
+    
+    def __init__(self,r_name):
+        self.r_name = r_name
+        self.lidar = Lidar(r_name)
+        self.r_motor = RotationalMotor(r_name,"right_wheel_motor")
+        self.l_motor = RotationalMotor(r_name,"left_wheel_motor")
+
+        self.cmd_vel_sub = rospy.Subscriber('cmd_vel',Twist,self.cmd_vel_callback)
+
+        self.max_vel = 6
+
+
+    def setup(self):
+        self.lidar.setup_device()
+        self.r_motor.setup_device()
+        self.l_motor.setup_device()
+        
+    def update(self,dt):
+        self.r_motor.update_device(dt)
+        self.l_motor.update_device(dt)
+
+    def cmd_vel_callback(self,twist):
+        fwd = twist.linear.x
+        angular = twist.angular.z
+
+        # TODO: this is a very simplified differential controller
+        if angular == 0:
+            self.set_forward_vel(fwd)
+        elif fwd == 0:
+            self.set_turn_vel(angular,0)
+        else:
+            self.set_turn_vel(angular,fwd/self.max_vel)
+
+    def set_forward_vel(self,vel):
+        self.r_motor.set_position(float("inf"))
+        self.l_motor.set_position(float("inf"))
+        self.r_motor.set_velocity(float(vel))
+        self.l_motor.set_velocity(float(vel))
+
+    def set_turn_vel(self,vel,throttle):
+        self.r_motor.set_position(float("inf"))
+        self.l_motor.set_position(float("inf"))
+
+        if vel < 0:
+            self.r_motor.set_velocity(float(-vel))
+            self.l_motor.set_velocity(float(min(throttle*-vel,-vel)))
+        else: 
+            self.r_motor.set_velocity(float(min(throttle*vel,vel)))
+            self.l_motor.set_velocity(float(vel))
