e68ae4614f825e477df9c467fcf3fe76e0a8ec4b
makspll <makspl17@gmail.com>
2021-02-07 17:05:16 +0000

    WIP

---
+++ ros-workspace/src/dr-phil/src/hardware/vision/door_handle_recognition.py
@@ -0,0 +1,12 @@
+
+
+def recognize_handle(img):
+    """ given image returns the bounding box (x1,x2,y1,y2) containing the handle, should always have regular spacing from the edges of the handles """
+    
+    x1 = 0
+    x2 = 0
+    y1 = 0
+    y2 = 0
+    
+    return (x1,x2,y1,y2)
+
