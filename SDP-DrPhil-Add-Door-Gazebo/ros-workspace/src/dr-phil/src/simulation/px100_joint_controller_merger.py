8b21b985358d08928e2eb1dad4c97ca4fea42ed4
hwixley <hwixley1@gmail.com>
2021-02-07 17:56:50 +0000

    Added room models and worlds for simulation testing

---
+++ SDP-DrPhil-Add-Door-Gazebo/ros-workspace/src/dr-phil/src/simulation/px100_joint_controller_merger.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float64MultiArray
+from std_msgs.msg import Float64
+
+class px100JointControllerMerger():
+    def __init__(self):    
+
+        # input, joints in order but first element is zero
+        # based on: http://www.inf.ed.ac.uk/teaching/courses/sdp/SDP2020/turtlebot3_docs.pdf
+        self.input_sub = rospy.Subscriber('joint_trajectory_point',Float64MultiArray,self.merge_callback) 
+
+        # output, splits input to those publishers in order
+        self.waist_pub = rospy.Publisher("/waist_controller/command",Float64,queue_size=10)
+        self.shoulder_pub = rospy.Publisher("/shoulder_controller/command",Float64,queue_size=10)
+        self.elbow_pub = rospy.Publisher("/elbow_controller/command",Float64,queue_size=10)
+        self.wrist_pub = rospy.Publisher("/wrist_controller/command",Float64,queue_size=10)
+        self.fingerL_pub = rospy.Publisher("/left_finger_controller/command",Float64,queue_size=10)
+        self.fingerR_pub = rospy.Publisher("/right_finger_controller/command",Float64,queue_size=10)
+        
+
+        self.state = [0,0,0,0,0,0]
+        self.gripper_in_open_pos = 1.57
+        self.gripper_in_closed_pos = -1.1
+
+        self.l_finger_out_open_pos = 0.037
+        self.l_finger_out_closed_pos = 0.015
+
+        self.r_finger_out_open_pos = -0.037
+        self.r_finger_out_closed_pos = -0.015   
+
+
+    def merge_callback(self,joint_trajectory_point):
+        """ distributes single message containing all arm joint configs requested to the multiple simulation controllers 
+            based on: http://www.inf.ed.ac.uk/teaching/courses/sdp/SDP2020/turtlebot3_docs.pdf
+        """
+        self.state = joint_trajectory_point.data
+        self.propagate_state()
+    
+
+    def interpolate(self,min_v,max_v,r):
+        """ returns v which is r of the way between min_v and max_v (0 > r < 1) 
+            min_v is < max_v 
+        """
+
+        # if min_v > max_v:
+        #     min_v,max_v = max_v,min_v
+        total = max_v - min_v
+        return (r * total) + min_v
+
+    def propagate_state(self):
+        """ distributes state to each controller based on self.state """
+
+        # first 4 need no changes
+        self.waist_pub.publish(self.state[1])
+        self.shoulder_pub.publish(self.state[2])
+        self.elbow_pub.publish(self.state[3])
+        self.wrist_pub.publish(self.state[4])
+        
+        # input only gives one value for gripper, interpolate both fingers
+        #           a         b
+        # closed --------in-------> open
+        # open ratio = a/(a+b)
+        a = abs(self.state[5] - self.gripper_in_closed_pos) 
+        total = (abs(self.gripper_in_open_pos) + abs(self.gripper_in_closed_pos))
+        open_ratio = a / total
+        # interpolate left finger motion
+        l_finger_pos = self.interpolate(self.l_finger_out_closed_pos,
+                                    self.l_finger_out_open_pos,
+                                    open_ratio)
+        
+        self.fingerL_pub.publish(l_finger_pos)
+        # right finger's range of motion is exactly opposite to that of l
+        self.fingerR_pub.publish(-l_finger_pos)
+
+
+
+# when started as a script
+if __name__ == '__main__':
+
+    # start up a new node
+    rospy.init_node('px100_joint_controller_merger',anonymous=True)
+
+    # initialize behaviour
+    merger = px100JointControllerMerger()
+
+    # update at 10 hz untill shutdown
+    rate = rospy.Rate(10) 
+
+    while not rospy.is_shutdown():
+        rate.sleep()
+
