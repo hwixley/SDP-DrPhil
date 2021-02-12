30d52992053732ac38310dd6b234812637a51b21
makspll <makspl17@gmail.com>
2021-02-12 05:23:47 +0000

    added behaviour for autonomous mapping of rooms and behaviours for managing processes

--- ros-workspace/src/dr-phil/src/hardware/trees.py
+++ ros-workspace/src/dr-phil/src/hardware/trees.py
@@ -1,8 +1,70 @@
+from time import time
 import py_trees
 import py_trees_ros
-from behaviours import ClosestObstacle,PublishTopic
+from behaviours import ClosestObstacle,PublishTopic,RunRos,KillSubprocess
 import operator
 from geometry_msgs.msg import Twist
+from visualization_msgs.msg import MarkerArray
+import os
+
+
+def create_explore_frontier_and_save_map(map_path=None):
+    """ creates subtree which executes frontier exploration, generates a map and saves it to the given map_name """
+    sequence = py_trees.composites.Sequence()
+
+    startExplorationNodes = create_run_ros_once(
+        launch_file="explore_task.launch",
+        package="dr-phil",
+        subprocess_variable_name="subprocess/explore")
+    
+    sequence2 = py_trees.composites.Sequence()
+
+    # frontierEmptyCheck = py_trees_ros.subscribers.CheckData(name="frontierEmpty?",
+    #                                     topic_name="explore/frontiers",
+    #                                     topic_type=MarkerArray,
+    #                                     variable_name="markers",
+    #                                     # comparison_operator=operator.__eq__,
+    #                                     # expected_value=[],
+    #                                     fail_if_bad_comparison=True,
+    #                                     fail_if_no_data=False)
+    sequence2 = py_trees.composites.Sequence()
+
+
+    idle = create_idle()
+    timeout = py_trees.decorators.Timeout(idle,duration=120)
+    inverter = py_trees.decorators.Inverter(timeout)
+    oneShotTimeout = py_trees.decorators.OneShot(inverter)
+    
+    map_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"map")if map_path is None else map_path  
+
+    saveMap = create_run_ros_once(node_file="map_saver",
+                                    package="map_server",
+                                    subprocess_variable_name="subprocess/map_saver",
+                                    args=["-f",map_path])
+    killExplorationNodes = KillSubprocess(name="killExplorationNodes",
+        subprocess_variable_name="subprocess/explore")
+
+    sequence2.add_children([oneShotTimeout,saveMap,killExplorationNodes])
+
+    sequence.add_children([startExplorationNodes,sequence2])
+
+    oneShot = py_trees.decorators.OneShot(sequence)
+    return oneShot
+
+def create_run_ros_once(package,subprocess_variable_name,launch_file=None,node_file=None,watch_time=5,time_out=10,args=[]):
+    """ creates subtree which attempts to launch a launchfile/node just once, after successful launch never runs again. 
+    After each failure will attempt to retry untill time out is reached """
+    launchBeh = RunRos(name="runLaunchfileOnce",
+                                launch_file=launch_file,
+                                node_file=node_file,
+                                package=package,
+                                subprocess_variable_name=subprocess_variable_name,
+                                watch_time=watch_time,
+                                args=args)
+    
+    oneShot = py_trees.decorators.OneShot(launchBeh)
+    timeOut = py_trees.decorators.Timeout(oneShot,duration=time_out)
+    return oneShot
 
 def create_idle(): 
     """ creates a subtree which causes the robot to idle on each tick, with status of RUNNING """
