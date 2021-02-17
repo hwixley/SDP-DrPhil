39ea55b754d518640e87fae9433693b86880a089
makspll <makspl17@gmail.com>
2021-02-17 02:13:00 +0000

    Modified mapping subtree and added new subtrees including create_set_positions_arm

--- ros-workspace/src/dr-phil/src/hardware/trees.py
+++ ros-workspace/src/dr-phil/src/hardware/trees.py
@@ -1,16 +1,73 @@
 from time import time
 import py_trees
+from py_trees.decorators import FailureIsRunning
+from py_trees.meta import oneshot
 import py_trees_ros
 from behaviours import ClosestObstacle,PublishTopic,RunRos,KillSubprocess
+from decorators import HoldStateForDuration
 import operator
 from geometry_msgs.msg import Twist
 from visualization_msgs.msg import MarkerArray
+from trajectory_msgs.msg import JointTrajectoryPoint
 import os
+import numpy as np
+from std_msgs.msg import Float64MultiArray
 
 
-def create_explore_frontier_and_save_map(map_path=None,timeout=120):
-    """ creates subtree which executes frontier exploration, generates a map and saves it to the given map_name """
+def create_frontier_empty_for_duration_check(duration=60):
+    """ creates subtree which returns SUCCESS if no data has been received on the `/explore/frontiers` topic for the given duration.
+        Returns FAILURE if data comes through in this time. RUNNING is propagated whenever waiting for message
+
+        Args:
+            duration: the time after which if no data is received on `/explore/frontiers` to return SUCCESS
+    """
+
+    fail_no_data = py_trees_ros.subscribers.WaitForData(name="FrontierDataArrived?",
+                                        topic_name="explore/frontiers",
+                                        topic_type=MarkerArray,
+                                        clearing_policy=py_trees.common.ClearingPolicy.ON_SUCCESS)
+
+    return HoldStateForDuration(fail_no_data,"HoldRunning",duration,state=py_trees.common.Status.RUNNING)
+
+
+    
+
+def create_set_positions_arm(parameters,name="positionArm"):
+
+    # clean inputs
+    lower_limits = [0, -1.57, -1.57, -1.57, -1.57, -1]
+    upper_limits = [0, 1.57, 1.57, 1.57, 1.57, 1.57]
+    clean_lower = max(lower_limits,parameters)
+    clean_upper = min(clean_lower,upper_limits)
+    parameters = list(clean_upper)
+
+    # create message
+    positions = Float64MultiArray()
+    positions.data = parameters 
+
+    position_arm = PublishTopic(
+        name=name,
+        msg=positions,
+        msg_type=Float64MultiArray,
+        topic="/joint_trajectory_point",
+        success_after_n_publishes=5,
+        queue_size=10
+    )
+
+    return position_arm
+
+def create_explore_frontier_and_save_map(map_path=None,timeout=120,no_data_timeout=20):
+    """ creates subtree which executes frontier exploration, generates a map and saves it to the given map_name 
+        Args:
+            map_path: the absolute path to where to save the map
+            timeout: the time after which to stop mapping overall
+            no_data_timeout: the time after which if we receive no new frontier data to consider the map to be complete
+    
+    """
     sequence = py_trees.composites.Sequence()
+
+    resetArmPosition = py_trees.decorators.OneShot(
+        create_set_positions_arm([0,0,-1.57,1.57,-1.57,-1],name="clearArmPosition"))
 
     startExplorationNodes = create_run_ros_once(
         launch_file="explore_task.launch",
@@ -18,23 +75,10 @@ def create_explore_frontier_and_save_map(map_path=None,timeout=120):
         subprocess_variable_name="subprocess/explore")
     
     sequence2 = py_trees.composites.Sequence()
+    oneshot_sequence2 = py_trees.decorators.OneShot(sequence2)
 
-    # frontierEmptyCheck = py_trees_ros.subscribers.CheckData(name="frontierEmpty?",
-    #                                     topic_name="explore/frontiers",
-    #                                     topic_type=MarkerArray,
-    #                                     variable_name="markers",
-    #                                     # comparison_operator=operator.__eq__,
-    #                                     # expected_value=[],
-    #                                     fail_if_bad_comparison=True,
-    #                                     fail_if_no_data=False)
-    sequence2 = py_trees.composites.Sequence()
+    frontierEmptyCheck = create_frontier_empty_for_duration_check(duration=no_data_timeout)
 
-
-    idle = create_idle()
-    timeout = py_trees.decorators.Timeout(idle,duration=timeout)
-    inverter = py_trees.decorators.Inverter(timeout)
-    oneShotTimeout = py_trees.decorators.OneShot(inverter)
-    
     map_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"map")if map_path is None else map_path  
 
     saveMap = create_run_ros_once(node_file="map_saver",
@@ -53,30 +97,31 @@ def create_explore_frontier_and_save_map(map_path=None,timeout=120):
         msg=haltMsg,
         msg_type=Twist,
         topic="/cmd_vel",
+        success_on_publish=True
     )
-    timeoutHalt = py_trees.decorators.Timeout(halt,duration=1)
 
-    sequence2.add_children([oneShotTimeout,saveMap,killExplorationNodes,timeoutHalt])
+    sequence2.add_children([frontierEmptyCheck,saveMap,killExplorationNodes,halt])
 
-    sequence.add_children([startExplorationNodes,sequence2])
+    sequence.add_children([resetArmPosition,startExplorationNodes,oneshot_sequence2])
 
-    oneShot = py_trees.decorators.OneShot(sequence)
-    return oneShot
+    timeout = py_trees.decorators.Timeout(sequence,duration=timeout)
 
+    return timeout
+
+@oneshot
 def create_run_ros_once(package,subprocess_variable_name,launch_file=None,node_file=None,watch_time=5,time_out=10,args=[]):
-    """ creates subtree which attempts to launch a launchfile/node just once, after successful launch never runs again. 
-    After each failure will attempt to retry untill time out is reached """
-    launchBeh = RunRos(name="runLaunchfileOnce",
+    """ creates subtree which attempts to launch a launchfile/node just once, after successful launch never runs again. """
+
+    runName = launch_file if launch_file is not None else node_file
+
+    launchBeh = RunRos(name="Run {0}, with args: {1}".format(runName,args),
                                 launch_file=launch_file,
                                 node_file=node_file,
                                 package=package,
                                 subprocess_variable_name=subprocess_variable_name,
                                 watch_time=watch_time,
                                 args=args)
-    
-    oneShot = py_trees.decorators.OneShot(launchBeh)
-    timeOut = py_trees.decorators.Timeout(oneShot,duration=time_out)
-    return oneShot
+    return launchBeh
 
 def create_idle(): 
     """ creates a subtree which causes the robot to idle on each tick, with status of RUNNING """
