30a51a63d5bacdb0334593f7e6a106e65a2a6802
makspll <makspl17@gmail.com>
2021-02-24 23:37:30 +0000

    Added test dependencies and setup each package as python module for external tests to be able to import them

---
+++ ros-workspace/src/dr_phil_gazebo/test/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import dr_phil_gazebo
+
+if __name__ == "__main__":
+    print("hello")
---
+++ ros-workspace/src/dr_phil_hardware/test/test.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+import dr_phil_hardware
+
+if __name__ == "__main__":
+    pass
