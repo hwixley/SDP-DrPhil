d4170567609dc9f0bacef7d50147e237e2727db0
andrei8782 <14292505+andrei8782@users.noreply.github.com>
2021-03-05 12:05:29 +0200

    wip testing

--- ros-workspace/src/dr_phil_hardware/test/unit_tests/lidar_test.py
+++ ros-workspace/src/dr_phil_hardware/test/unit_tests/lidar_test.py
@@ -9,13 +9,16 @@ import roslib; roslib.load_manifest(PKG)
 # only requirement is that we return results in an appropriate xml format 
 # see: http://wiki.ros.org/unittest
 
-class TestExample(unittest.TestCase):
+class LidarTest(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(1,1)
+    
+    def test_get_unit_vec_from_dir(self):
+        pass
 
 if __name__ == "__main__":
     import rosunit
-    rosunit.unitrun(PKG,'test_example',TestExample)
+    rosunit.unitrun(PKG,'lidar_test',LidarTest)
 
 # to run me, individually: just execute me ./test.py 
 # to run all the tests listed in CMakeLists.txt: catkin_make run_tests_dr_phil_hardware_rostest
