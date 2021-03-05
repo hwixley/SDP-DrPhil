df528f05706b5477a3e3c172cecb97b880b195b5
andrei8782 <14292505+andrei8782@users.noreply.github.com>
2021-03-05 11:57:10 +0200

    Refactoring

--- ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/lidar.py
+++ ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/lidar.py
@@ -40,7 +40,7 @@ class Lidar:
         dir = np.append(dir,np.array([[1]]),axis=0)
         robot_origin_rf = self.extrinsic_mat_inv @ np.array([[0],[0],[0],[1]])
 
-        ray = Ray(robot_origin_rf[:-1,:],((self.extrinsic_mat_inv[:,:-1] @ dir[:-1,:]) ))
+        ray = Ray(robot_origin_rf[:-1,:],(self.extrinsic_mat_inv[:,:-1] @ dir[:-1,:]) )
         return ray
     
     def get_ray_projection(self, ray):
@@ -51,25 +51,42 @@ class Lidar:
         dir[-1] = 0
         return Ray(origin, dir)
     
-    def get_camera_ray_length(self, ray, data):
-        """ returns a ray's length by averaging readings from the lidar sensor
+    def get_corresponding_lidar_rays(self, camera_ray, data):
+        """ returns the 2 corresponding lidar rays to a camera ray
+            given in the lidar frame
             Args:
-                ray: camera ray that must be in the plane formed by lidar rays
-                data: lidar data
+                camera_ray: camera ray in lidar frame
+                data: LaserScan data
         """
-
         angle = data.angle_min
+        angle_deg = np.rad2deg(angle)
         while angle <= data.angle_max:
             lidar_ray1 = get_unit_vec_from_dir(angle)
             lidar_ray2 = get_unit_vec_from_dir((angle + data.angle_increment) % (2*math.pi))
             subtr = subtract(lidar_ray1, lidar_ray2)
             
             if intersect(subtr, ray):
-                return (data.ranges[i] + data.ranges[i + 1]) / 2
+                lidar_ray1.length = data.ranges[angle_deg]
+                lidar_ray2.length = data.ranges[(angle_deg + np.rad2deg(data.angle_increment)) % 360]
+                return lidar_ray1, lidar_ray2
             
             angle += data.angle_increment
+            angle_deg = np.rad2deg(angle)
 
-        return 0
+        return None, None
+
+    def get_camera_ray_length(self, camera_ray, data):
+        """ returns a ray's length by averaging readings from the lidar sensor
+            Args:
+                camera_ray: camera ray that must be in the plane formed by lidar rays
+                data: lidar data
+        """
+
+        lidar_ray1, lidar_ray2 = get_corresponding_lidar_rays(camera_ray, data)
+        if lidar_ray1 != None:
+            return (lidar_ray1.length + lidar_ray2.length) / 2
+        else:
+            return None
 
     def get_normal_to_plane(self, l_ray1, l_ray2):
         """ returns the normal to the plane formed by the 2 lidar rays 
@@ -82,20 +99,24 @@ class Lidar:
         horizontal_normal_endpoint = np.array([[0],[0],[1]])
 
         v = l_ray1.dir
-        normalized_v = v / np.sqrt(np.sum(v**2))
+        normalized_v = v.dir / np.sqrt(np.sum(v.dir**2))
         l_ray1_endpoint = l_ray1.origin + normalized_v ** l_ray1.length
 
         u = l_ray2.dir
-        normalized_u = u / np.sqrt(np.sum(u**2))
+        normalized_u = u.dir / np.sqrt(np.sum(u.dir**2))
         l_ray2_endpoint = l_ray2.origin + normalized_u ** l_ray2.length
 
         subtr = np.subtract(l_ray1_endpoint, l_ray2_endpoint)
-        
-        normal_dir = np.cross(subtr / 2, horizontal_normal_endpoint)
-        normal_origin = l_ray2_endpoint + subtr / 2
+        normalized_subtr = subtr.dir / np.sqrt(np.sum(subtr.dir**2))
+        halved_subtr_origin = subtr.origin + (normalized_subtr * subtr.length) / 2
+        halved_subtr_dir = halved_subtr_origin + (normalized_subtr * subtr.length) / 2
+        halved_subtr = Ray(halved_subtr_origin, halved_subtr_dir, subtr.length / 2)
+
+        normal_dir = np.cross(halved_subtr.dir, horizontal_normal_endpoint)
+        normal_origin = halved_subtr.origin
         normal_length = 1
         
-        """ normal_dir might need to be ** -1 """"
+        """ normal_dir might need to be * -1 """"
         scalar_proj_of_normal_on_v = np.dot(normal_dir - normal_origin, v) / np.linalg.norm(v)
    
         if scalar_proj_of_normal_on_v[0] > 0:
@@ -108,6 +129,6 @@ class Lidar:
     def get_unit_vec_from_dir(angle):
         """ return unit vector given an angle, counterclockwise from x-axis """
         origin = np.array([[0],[0],[0]]
-        dir = np.array([[math.cos(angle)], [math.sin(angle)]])
+        dir = np.array([[math.cos(angle)], [math.sin(angle)], [0]])
         length = 1
         return Ray(origin, dir, length)
--- ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/utils.py
+++ ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/utils.py
@@ -27,7 +27,7 @@ def intersect(segment, camera_ray):
     """ returns true if 2D rays intersect, false otherwise """
     base_start = tuple(segment.origin[:2])
     base_end = tuple(segment.origin[:2] + segment.dir[:2]) 
-    segment = LineString([base_start, base_end])
+    base_segment = LineString([base_start, base_end])
 
     camera_ray_start = tuple(camera_ray.origin[:2])
     tmp_camera_ray = np.subtract(camera_ray.dir[:2], camera_ray.origin[:2])
@@ -37,14 +37,14 @@ def intersect(segment, camera_ray):
     camera_ray_end = tuple(camera_ray_start + 2 * data.range_max * unit_camera_ray)
     camera_ray = LineString([camera_ray_start, camera_ray_end])
 
-    return camera_ray.intersects(segment)
+    return camera_ray.intersects(base_segment)
 
 def subtract(ray1, ray2):
     """ returns ray1 - ray2
     the 2 rays have the same origin """
     
     origin = ray2.origin + ray2.dir
-    dir = np.subtract(ray.origin + ray1.dir, ray2.origin + ray2.dir)
+    dir = np.subtract(ray1.origin + ray1.dir, ray2.origin + ray2.dir)
     length = math.sqrt(ray1.length ** 2 + ray2.length ** 2)
 
     return Ray(origin, dir, length)
