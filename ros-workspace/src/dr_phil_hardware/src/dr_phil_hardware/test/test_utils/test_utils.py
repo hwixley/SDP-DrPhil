6d25d61aca3149cd56eae90bf2de47ea3b4e7c53
makspll <makspl17@gmail.com>
2021-03-07 10:40:05 +0000

    moved test code to src

---
+++ ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/test/test_utils/test_utils.py
@@ -0,0 +1,12 @@
+
+
+import unittest
+import math
+import numpy as np
+
+
+
+def assertRayEquals(test,r_correct,r_true,rel_tol=1e-09,abs_tol=1e-3,msg=""):
+    """ asserts that the two rays are approximately the same based on their distance as points"""
+    test.assertTrue(math.isclose(np.linalg.norm(r_true.get_vec()-r_correct.get_vec()),0,abs_tol=abs_tol,rel_tol=rel_tol)
+        ,"camera ray was off by {}. {}".format(np.linalg.norm(r_true.get_vec()-r_correct.get_vec()),msg))
