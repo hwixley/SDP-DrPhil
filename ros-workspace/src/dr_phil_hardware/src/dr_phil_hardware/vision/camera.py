dcccf3963a93f02c64fe38094cf246fe61e20c56
makspll <makspl17@gmail.com>
2021-03-07 12:25:18 +0000

    fixed error in get_ray_in_robot_frame introduced in previous commit

--- ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/camera.py
+++ ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/camera.py
@@ -5,6 +5,7 @@ import dr_phil_hardware.vision.utils as utils
 from dr_phil_hardware.vision.ray import Ray
 from image_geometry import PinholeCameraModel
 import tf
+
 import rospy
 
 class Camera:
@@ -16,8 +17,7 @@ class Camera:
         """
         self.camera = PinholeCameraModel()
         self.camera.fromCameraInfo(camera_info)
-        print("\nfx:{},fy:{},cx:{},cy:{},Tx:{},Ty:{}".format(self.camera.fx(),self.camera.fy(),self.camera.cx(),self.camera.cy(),self.camera.Tx(),self.camera.Ty()))
-        print("\nxz:{},yz{}".format(self.camera.cx()+self.camera.Tx(),self.camera.cy()+self.camera.Ty()))
+
     def get_frame_id(self):
         return self.camera.tfFrame()
 
@@ -29,6 +29,17 @@ class Camera:
         self.extrinsic_mat = rob2cam
         self.extrinsic_mat_inv = utils.invert_homog_mat(rob2cam)
 
+        # for transforming directions
+        self.extrinsic_mat_inv_no_trans = np.copy(self.extrinsic_mat_inv)
+        self.extrinsic_mat_inv_no_trans[:-1,3]  = 0
+
+    def get_pixel_through_ray(self,ray : Ray):
+        """ assumes ray is going through origin of camera """
+
+        # notice pixels are flipped
+        (u,v) = self.camera.project3dToPixel(tuple(ray.get_vec()))
+        return np.array([u,v])
+
     def get_ray_through_image(self,img_pos):
         """ returns the direction vector in camera space of the ray 
             passing through the camera center and all the 3D points corresponding to the given 2D point on the image  
@@ -36,20 +47,26 @@ class Camera:
             Args:
                 img_pos (array_like): must be of length 2, the pixel coordinate (with origin in top left corner)
         """
+
+        
         (u,v) = np.asarray(img_pos).flatten()
+        ray_3d = self.camera.projectPixelTo3dRay((u,v))
+
         return Ray(np.array([[0],[0],[0]]),
-            np.array(self.camera.projectPixelTo3dRay((u,v))).reshape((3,1)),length=1)
+            np.array(np.array(ray_3d).reshape((3,1))),length=1)
 
     def get_ray_in_robot_frame(self,ray : Ray) -> Ray: 
         """ transforms ray () from camera to robot space """
         
         dir = ray.dir
         dir = np.append(dir,np.array([[1]]),axis=0)
-        dir = (self.extrinsic_mat_inv @ dir)
-        dir = dir[:-1,:]
+
+        # we rotate the direction but not translate it
+        dir = (self.extrinsic_mat_inv_no_trans @ dir)[:-1,:]
 
         cam_origin_rf = self.extrinsic_mat_inv @ np.array([[0],[0],[0],[1]])
-        
+
         ray = Ray(cam_origin_rf[:-1,:],dir,length=1)
+          
         return ray
 
