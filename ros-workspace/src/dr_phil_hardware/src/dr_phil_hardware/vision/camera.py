f1a08fc95e3cb0a0e9705dba0b3b3cb2c2348d83
makspll <makspl17@gmail.com>
2021-03-07 10:41:08 +0000

    changed camera.py and ray.py

--- ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/camera.py
+++ ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/camera.py
@@ -6,40 +6,28 @@ from dr_phil_hardware.vision.ray import Ray
 from image_geometry import PinholeCameraModel
 import tf
 import rospy
-from tf2_msgs.msg import TFMessage
 
 class Camera:
 
-    def __init__(self,camera_info,robot_frame="/base_link"):
+    def __init__(self,camera_info):
         """ 
             Args:
                 camera_info: the camera_info message published by the camera 
         """
         self.camera = PinholeCameraModel()
         self.camera.fromCameraInfo(camera_info)
+        print("\nfx:{},fy:{},cx:{},cy:{},Tx:{},Ty:{}".format(self.camera.fx(),self.camera.fy(),self.camera.cx(),self.camera.cy(),self.camera.Tx(),self.camera.Ty()))
+        print("\nxz:{},yz{}".format(self.camera.cx()+self.camera.Tx(),self.camera.cy()+self.camera.Ty()))
+    def get_frame_id(self):
+        return self.camera.tfFrame()
+
+    def setup_transform(self,rob2cam):
+        """
+            sets up transform from camera to robot 
+        """
         
-        self.camera_transform_listener = tf.TransformListener()
-        self.tf_active_sub = rospy.Subscriber("/tf",TFMessage,self.setup_transform)
-        self.transformerRos = tf.TransformerROS()
-
-        
-    def setup_transform(self,data):
-
-        (trans,rot) = self.camera_transform_listener.lookupTransform(
-            self.camera.tfFrame(),
-            '/base_link',
-            rospy.Time(0))
-
-        try:
-            transform = self.transformerRos.fromTranslationRotation(trans,rot)
-        except (tf.ConnectivityException,tf.LookupException,tf.ExtrapolationException):
-            print("Could not find camera transform!")
-
-        self.extrinsic_mat = transform
-        self.extrinsic_mat_inv = utils.invert_homog_mat(transform)
-
-        # unregister callback (run once only)
-        self.tf_active_sub.unregister()
+        self.extrinsic_mat = rob2cam
+        self.extrinsic_mat_inv = utils.invert_homog_mat(rob2cam)
 
     def get_ray_through_image(self,img_pos):
         """ returns the direction vector in camera space of the ray 
@@ -49,17 +37,19 @@ class Camera:
                 img_pos (array_like): must be of length 2, the pixel coordinate (with origin in top left corner)
         """
         (u,v) = np.asarray(img_pos).flatten()
-
         return Ray(np.array([[0],[0],[0]]),
-            np.array(self.camera.projectPixelTo3dRay((u,v))).reshape((3,1)))
+            np.array(self.camera.projectPixelTo3dRay((u,v))).reshape((3,1)),length=1)
 
-    def get_ray_in_robot_frame(self,ray):
+    def get_ray_in_robot_frame(self,ray : Ray) -> Ray: 
         """ transforms ray () from camera to robot space """
         
         dir = ray.dir
         dir = np.append(dir,np.array([[1]]),axis=0)
-        cam_origin_rf = self.extrinsic_mat_inv @ np.array([[0],[0],[0],[1]])
+        dir = (self.extrinsic_mat_inv @ dir)
+        dir = dir[:-1,:]
 
-        ray = Ray(cam_origin_rf[:-1,:],((self.extrinsic_mat_inv[:,:-1] @ dir[:-1,:]) ))
+        cam_origin_rf = self.extrinsic_mat_inv @ np.array([[0],[0],[0],[1]])
+        
+        ray = Ray(cam_origin_rf[:-1,:],dir,length=1)
         return ray
 
--- ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/ray.py
+++ ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/ray.py
@@ -1,9 +1,18 @@
 import math
+import numpy as np
 
 class Ray():
     def __init__(self,origin, dir, length = math.inf):
         self.origin = origin
-        self.dir = dir
+        self.dir = dir / np.linalg.norm(dir)
         self.length = length
+
+        if not (origin.size == dir.size):
+            raise ValueError("The origin and direction have different dimensionality: {} and {}".format(origin.size,dir.size))
+
+    def get_vec(self):
+        """ returns ray as vector from origin in direction of the ray's length """
+        return ((self.dir * self.length) + self.origin) - self.origin
+
     def __repr__(self):
         return "Ray:"+str(self.origin) + "\n->\n" + str(self.dir) + "\nLength: " + str(self.length) + "\n"
