a8ba2c00497aa898ea0ff0f95eb18247b8a5a5d1
hwixley <hwixley1@gmail.com>
2021-03-07 16:42:22 +0000

    Coordinate transformations using the input vector complete

--- ros-workspace/src/dr_phil_gazebo/src/dr_phil_gazebo/calc-spray-path.py
+++ ros-workspace/src/dr_phil_gazebo/src/dr_phil_gazebo/calc-spray-path.py
@@ -22,37 +22,62 @@ class LineGraph:
     def __init__(self, vector):
         self.vector = vector
 
-        if self.vector.x == 0:
-            self.gradient = float("inf")
+        if self.vector.z == 0:
+            self.gradient = float("inf")  # Vertical lines have infinity gradient
         else:
             self.gradient = self.vector.x / self.vector.z
 
+        # Segments correspond to CAST plot and are used to determine the rotation direction
         if vector.x >= 0:
             if vector.z >= 0:
                 self.segment = 0
             else:
-                self.segment = 3
+                self.segment = 1
         elif vector.x < 0:
             if vector.z >= 0:
-                self.segment = 1
+                self.segment = 3
             else:
                 self.segment = 2
 
 
-# Line1 represents current axes and Line2 represents destination axes
+# Calculates the rotation angle (including direction) between two lines
+# PARAMS: Line1 represents current axes, Line2 represents destination axes
 def calc_rotation_angle(line1, line2):
     angle = math.atan(line2.gradient) - math.atan(line1.gradient)
 
-    # Changes angle depending on whet
-    if not ((line1.gradient < 0 and line2.gradient < 0) or (line1.gradient > 0 and line2.gradient > 0)):
-        angle = math.pi - abs(angle)
-        if not ((line1.segment == 1 and line2.segment == 3) or line1.segment > line2.segment):
-            angle = -1 * angle
+    if not ((line1.segment in [0, 3] and line2.segment in [0, 3]) or (
+            line1.segment in [1, 2] and line2.segment in [1, 2])) \
+            or (line1.segment != line2.segment and angle == 0):
+        angle = math.pi - angle
+
+    if not ((line1.segment == line2.segment and line1.gradient < 0 and line1.gradient < line2.gradient) or \
+            (line1.segment == line2.segment and line1.gradient > 0 and line1.gradient > line2.gradient) or \
+            (line1.segment == 3 and line2.segment == 0) or (line1.segment < line2.segment)):
+        print("neg")
+        angle = -1 * abs(angle)
+
+    if line2.segment in [1,2]:
+        angle = 2*math.pi - angle
 
     return math.degrees(angle)
 
+# Transform the points given the angle of rotation
+def transform_points(points, center, vector):
+    vector_line = LineGraph(vector)
+    origin_line = LineGraph(ORIGIN)
+    angle = calc_rotation_angle(origin_line, vector_line)
 
-# Given the center point and height to spray we can calculate the spray centroids
+    center_matrix = np.empty((2, 3))
+    center_matrix[0, :] = center.z
+    center_matrix[1, :] = center.x
+
+    c, s = np.cos(angle), np.sin(angle)
+    rotation_matrix = np.array(((c, -s), (s, c)))
+
+    return np.matmul(rotation_matrix, points - center_matrix) + center_matrix
+
+
+# Calculates y coordinates by starting at the center and going up and down the handle until the handle has been covered
 def calc_y_spray_centroids(center_y):
     top = (center_y + HANDLE_DIMENSIONS[1] / 2) + SPRAY_CONFIDENCE
     bottom = (center_y - HANDLE_DIMENSIONS[1] / 2) - SPRAY_CONFIDENCE
@@ -76,32 +101,31 @@ def calc_y_spray_centroids(center_y):
     return spray_centroids
 
 
+# Calculates the xz locations around the handle for spraying
 def calc_xz_spray_centroids(center, vector):
-    xz_coords = np.empty((3, 2))
-    xz_coords[0, :] = [center.x - DISTANCE_FROM_HANDLE, center.z]
-    xz_coords[1, :] = [center.x, center.z - DISTANCE_FROM_HANDLE]
-    xz_coords[2, :] = [center.x, center.z + DISTANCE_FROM_HANDLE]
+    xz_coords = np.empty((2, 3))
+    xz_coords[:, 0] = [center.x - DISTANCE_FROM_HANDLE, center.z]
+    xz_coords[:, 1] = [center.x, center.z - DISTANCE_FROM_HANDLE]
+    xz_coords[:, 2] = [center.x, center.z + DISTANCE_FROM_HANDLE]
 
-    # Transform the points given the angle of rotation
-    vector_line = LineGraph(vector)
-    origin_line = LineGraph(ORIGIN)
-    angle = calc_rotation_angle(origin_line, vector_line)
-    print(angle)
-    return xz_coords
+    new_points = transform_points(xz_coords, center, vector)
+
+    return new_points
 
 
+# Compiles xyz coordinates into a matrix
 def calc_coords(center, vector):
     xz_coords = calc_xz_spray_centroids(center, vector)
     y_coords = calc_y_spray_centroids(center.y)
 
-    coords = np.empty((len(y_coords) * len(xz_coords), 3))
+    coords = np.empty(((len(y_coords) * xz_coords.shape[1]), 3))
 
     row = 0
-    for a in range(len(xz_coords)):
+    for a in range(xz_coords.shape[1]):
         for s in range(len(y_coords)):
-            coords[row, 0] = xz_coords[a, 0]
+            coords[row, 0] = xz_coords[0, a]
             coords[row, 1] = y_coords[s]
-            coords[row, 2] = xz_coords[a, 1]
+            coords[row, 2] = xz_coords[1, a]
             row += 1
 
     return coords
@@ -120,11 +144,7 @@ def main(center, vector):
 
 if __name__ == '__main__':
     ORIGIN = Coord(0, 0, 0)
-
     center = Coord(0.0, 0.0, 0.0)
+    direction = Coord(1.0, 0.0, 0.0)
 
-    direction = Coord(1.0, 0.0, -1.0)
     main(center, direction)
-
-    direction = Coord(0.0, 0.0, 0.0)
-    # main(center, direction)
