7d98ddc4fb9ffa091b1ca14e0f87436ac254d795
makspll <makspl17@gmail.com>
2021-03-10 06:47:05 +0000

    added error checking to ray.py

--- ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/ray.py
+++ ros-workspace/src/dr_phil_hardware/src/dr_phil_hardware/vision/ray.py
@@ -4,19 +4,35 @@ import numpy as np
 class Ray():
     def __init__(self,origin, dir, length = math.inf):
         self.origin = origin
-        self.dir = dir / np.linalg.norm(dir)
+
+        dir_length = np.linalg.norm(dir)
+        self.dir = dir / dir_length if dir_length else dir
+
         self.length = length
 
         if not (origin.size == dir.size):
             raise ValueError("The origin and direction have different dimensionality: {} and {}".format(origin.size,dir.size))
 
     def get_point(self):
-        """ converts the ray to a point, with origin at the rays origin  (i.e. in the frame of the ray)"""
+        """ converts the ray to a point, with origin at the rays origin  (i.e. in the frame of the ray)
+        
+            Raises:
+                ValueException: if length is not finite
+        """
+        if not math.isfinite(self.length):
+            raise ValueError("Cannot convert infinite ray to point")
+
         return ((self.dir * self.length) + self.origin)
 
     def get_vec(self):
-        """ returns ray as vector from origin in direction of the ray's length """
-        return self.get_point() - self.origin
+        """ returns ray as vector from origin in direction of the ray's length 
+                
+            Raises:
+                ValueError: if length is not finite"""
+        if not math.isfinite(self.length):
+            raise ValueError("Cannot convert infinite ray to vector")
+
+        return self.get_point() - self.origin 
 
     def __repr__(self):
         return "Ray:"+str(self.origin) + "\n->\n" + str(self.dir) + "\nLength: " + str(self.length) + "\n"
@@ -24,15 +40,15 @@ class Ray():
 
     def get_transformed(self,transformation):
         """ returns new transformed ray using the provided transformation """
-        
+
         dir = self.dir
-        dir = np.append(dir,np.array([[1]]),axis=0)
-
+        
         # we rotate the direction but not translate it (i.e. dont use the translation column)
-        dir = (transformation[:,:-1] @ dir[:-1,:])[:-1,:]
+        dir = (transformation[:,:-1] @ dir)[:-1,:]
 
-        cam_origin_rf = transformation @ np.array([[0],[0],[0],[1]])
+        origin_homog = np.append(self.origin,np.array([[1]]),axis=0)
+        new_origin = transformation @ origin_homog
 
-        ray = Ray(cam_origin_rf[:-1,:],dir,length=1)
+        ray = Ray(new_origin[:-1,:],dir,length=self.length)
 
         return ray
