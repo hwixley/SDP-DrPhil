9f8e9ec9a8cf096a1b5c7e385fb43f652f550cde
makspll <makspl17@gmail.com>
2021-03-27 17:39:31 +0000

    hopefully fixes to pcl merging

--- ros-workspace/src/dr_phil_gazebo/src/dr_phil_gazebo/coverage_pc_generator.cpp
+++ ros-workspace/src/dr_phil_gazebo/src/dr_phil_gazebo/coverage_pc_generator.cpp
@@ -9,84 +9,113 @@
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <sstream>
 #include <std_msgs/Float32.h>
+#include <mutex>         
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "geometry_msgs/TransformStamped.h"
+
+#define FIXED_FRAME "map"
 
 class SubscribeAndPublish
 {
 
 sensor_msgs::PointCloud MergedCloud;
 int n=0;
-tf::StampedTransform transform;
-tf::TransformListener listener;
+tf2_ros::Buffer *tfBuffer;
+tf2_ros::TransformListener *tfListener;
+std::mutex mutex_;
+float history_period = 10;
 
 public:
+
+
+  void publish_merged(){
+    std::unique_lock<std::mutex> lck(mutex_);
+    if (MergedCloud.points.size() > 0){
+      MergedCloud.header.frame_id = FIXED_FRAME;
+      pub_.publish(MergedCloud);
+    } else {
+      ROS_INFO_THROTTLE(1,"empty merged cloud");
+    }
+    lck.unlock();
+  }
+
   SubscribeAndPublish()
   {
-    
+    // keep history of transforms up to 10 seconds
+    tfBuffer = new tf2_ros::Buffer(ros::Duration(history_period));
+    tfListener = new tf2_ros::TransformListener(*tfBuffer);
+
     pub_ = n_.advertise<sensor_msgs::PointCloud>("/pointCloud", 10);
-    sub1_ = n_.subscribe("/spray_controller/command", 1, &SubscribeAndPublish::callback1, this);
+    sub1_ = n_.subscribe("/spray_controller/command", 100, &SubscribeAndPublish::callback1, this);
     
 
+    // publish merged occasionaly
+    ros::Rate r(20);
 
-    // try{
-    //     listener.waitForTransform("odom", "px100/led_link1", ros::Time::now(), ros::Duration(5));
-    //     listener.lookupTransform("odom", "px100/led_link1", ros::Time(), transform);
-    // }
-    // catch (tf::TransformException ex){
-    //     ROS_ERROR("%s",ex.what());
-    // }
+    while(ros::ok()){
+      publish_merged();
+      ros::spinOnce();
+      r.sleep();
+    }
+
   }
 
   void callback1(const std_msgs::Float32 &msg_range)
   {
     if (msg_range.data ==1){
-  
-      sub_ = n_.subscribe("/depth_camera/depth/points", 10, &SubscribeAndPublish::callback, this);
-      subL_ = n_.subscribe("/depth_left/depth/points", 10, &SubscribeAndPublish::callbackLeftNozzle, this);
-      subR_ = n_.subscribe("/depth_right/depth/points", 10, &SubscribeAndPublish::callbackRightNozzle, this);
+      ROS_INFO("Enabling nozzle subscribers");
+      sub_ = n_.subscribe("/depth_camera/depth/points", 3, &SubscribeAndPublish::merge_callback, this);
+      subL_ = n_.subscribe("/depth_left/depth/points", 3, &SubscribeAndPublish::merge_callback, this);
+      subR_ = n_.subscribe("/depth_right/depth/points", 3, &SubscribeAndPublish::merge_callback, this);
     }
     
     if (msg_range.data ==0){
+      ROS_INFO("Disabling nozzle subscribers");
       sub_.shutdown();
       subL_.shutdown();
       subR_.shutdown();
+
     }
   }
   
   
-  void callback(const boost::shared_ptr<const sensor_msgs::PointCloud2> &input)
-  {
 
-    
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    sensor_msgs::PointCloud inputCloud;
-    sensor_msgs::PointCloud tempCloud;   
-    sensor_msgs::PointCloud2 test;
-    
-    //convert to PCL pointcloud then transform to global frame
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*input, pcl_pc2);
-    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
 
-    try{
-        listener.waitForTransform("odom", "px100/led_link1", input->header.stamp, ros::Duration(5));
-        listener.lookupTransform("odom", "px100/led_link1", input->header.stamp, transform);
+
+  void merge_callback(const boost::shared_ptr<const sensor_msgs::PointCloud2> &input){
+
+    // if cloud is older than we have transforms for
+    if (input->header.stamp <= (ros::Time::now() - ros::Duration(history_period))){
+        ROS_INFO("Ignoring point cloud older than 10 seconds");
+        return;
     }
-    catch (tf::TransformException ex){
+
+    // tf::StampedTransform transform;
+    geometry_msgs::TransformStamped transformStamped;
+
+    ROS_INFO("cloud arrived from timestamp: %f",input->header.stamp.toSec());
+    try{
+        // tfBuffer->waitForTransform(FIXED_FRAME, input->header.frame_id, input->header.stamp, ros::Duration(5));
+        transformStamped = tfBuffer->lookupTransform(FIXED_FRAME,input->header.frame_id , input->header.stamp);
+
+    }
+    catch (tf2::TransformException ex){
         ROS_ERROR("%s",ex.what());
-        ROS_ERROR("middle transform");
     }
-
-
-    pcl_ros::transformPointCloud("odom", *temp_cloud, *cloud_transformed,listener);
     sensor_msgs::PointCloud2 cloud_publish;
-    pcl::toROSMsg(*cloud_transformed,cloud_publish);
+    tf2::doTransform((sensor_msgs::PointCloud2)*input, cloud_publish,transformStamped);
 
     
     //convert to old pointcloud type to merge latest pointcloud with 
     //larger cloud being built up
+    sensor_msgs::PointCloud inputCloud; 
     sensor_msgs::convertPointCloud2ToPointCloud(cloud_publish, inputCloud);
-    
+
+    inputCloud.header.frame_id = input->header.frame_id;
+    inputCloud.header.stamp = input->header.stamp;
 
     for (int p=0; p<inputCloud.points.size(); ++p)
     {
@@ -97,128 +126,25 @@ public:
         --p;
       }
     }
-    
+
+    ROS_INFO_THROTTLE(0.5,"new cloud points: %lu, frame: %s",inputCloud.points.size(),input->header.frame_id.c_str());
+    // prevent other threads modifying merged cloud 
+    std::unique_lock<std::mutex> lck(mutex_);
+
     if(n==0){
       MergedCloud = inputCloud;
     }
-
+ 
     //publish to topic
+    sensor_msgs::PointCloud tempCloud;  
     merge_point_cloud(inputCloud, MergedCloud,tempCloud);
     MergedCloud = tempCloud;
-    pub_.publish(MergedCloud);
+
+    // now fair game to modify
+    lck.unlock();
+
     n++;
-  }
-  
-  void callbackRightNozzle(const boost::shared_ptr<const sensor_msgs::PointCloud2> &input)
-  {
 
-    
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    sensor_msgs::PointCloud inputCloud;
-    sensor_msgs::PointCloud tempCloud;   
-    sensor_msgs::PointCloud2 test;
-    
-    //convert to PCL pointcloud then transform to global frame
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*input, pcl_pc2);
-    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
-
-    try{
-        listener.waitForTransform("odom", "px100/led_right_link1", input->header.stamp, ros::Duration(5));
-        listener.lookupTransform("odom", "px100/led_right_link1", input->header.stamp, transform);
-    }
-    catch (tf::TransformException ex){
-        ROS_ERROR("right transform%s",ex.what());
-        ROS_ERROR("right transform");
-    }
-
-
-    pcl_ros::transformPointCloud("odom", *temp_cloud, *cloud_transformed,listener);
-    sensor_msgs::PointCloud2 cloud_publish;
-    pcl::toROSMsg(*cloud_transformed,cloud_publish);
-
-    
-    //convert to old pointcloud type to merge latest pointcloud with 
-    //larger cloud being built up
-    sensor_msgs::convertPointCloud2ToPointCloud(cloud_publish, inputCloud);
-    
-
-    for (int p=0; p<inputCloud.points.size(); ++p)
-    {
-      if (isnan(inputCloud.points[p].x))
-      {
-        inputCloud.points[p] = inputCloud.points[inputCloud.points.size()-1];
-        inputCloud.points.resize(inputCloud.points.size()-1);
-        --p;
-      }
-    }
-    
-    if(n==0){
-      MergedCloud = inputCloud;
-    }
-
-    //publish to topic
-    merge_point_cloud(inputCloud, MergedCloud,tempCloud);
-    MergedCloud = tempCloud;
-    pub_.publish(MergedCloud);
-    n++;
-  }
-  
-  void callbackLeftNozzle(const boost::shared_ptr<const sensor_msgs::PointCloud2> &input)
-  {
-
-    
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    sensor_msgs::PointCloud inputCloud;
-    sensor_msgs::PointCloud tempCloud;   
-    sensor_msgs::PointCloud2 test;
-    
-    //convert to PCL pointcloud then transform to global frame
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*input, pcl_pc2);
-    pcl::fromPCLPointCloud2(pcl_pc2,*temp_cloud);
-
-    try{
-        listener.waitForTransform("odom", "px100/led_left_link1", input->header.stamp, ros::Duration(5));
-        listener.lookupTransform("odom", "px100/led_left_link1", input->header.stamp, transform);
-    }
-    catch (tf::TransformException ex){
-        ROS_ERROR("left transform%s",ex.what());
-        ROS_ERROR("left transform");
-    }
-
-
-    pcl_ros::transformPointCloud("odom", *temp_cloud, *cloud_transformed,listener);
-    sensor_msgs::PointCloud2 cloud_publish;
-    pcl::toROSMsg(*cloud_transformed,cloud_publish);
-
-    
-    //convert to old pointcloud type to merge latest pointcloud with 
-    //larger cloud being built up
-    sensor_msgs::convertPointCloud2ToPointCloud(cloud_publish, inputCloud);
-    
-
-    for (int p=0; p<inputCloud.points.size(); ++p)
-    {
-      if (isnan(inputCloud.points[p].x))
-      {
-        inputCloud.points[p] = inputCloud.points[inputCloud.points.size()-1];
-        inputCloud.points.resize(inputCloud.points.size()-1);
-        --p;
-      }
-    }
-    
-    if(n==0){
-      MergedCloud = inputCloud;
-    }
-
-    //publish to topic
-    merge_point_cloud(inputCloud, MergedCloud,tempCloud);
-    MergedCloud = tempCloud;
-    pub_.publish(MergedCloud);
-    n++;
   }
 
 private:
@@ -260,10 +186,10 @@ else
 bool merge_point_cloud(const sensor_msgs::PointCloud& cloud_in1, const sensor_msgs::PointCloud& cloud_in2, sensor_msgs::PointCloud& cloud_out)
 {
     // check if both have the same frame_id
-    if(cloud_in1.header.frame_id != cloud_in2.header.frame_id)
-        return false;
+    // if(cloud_in1.header.frame_id != cloud_in2.header.frame_id)
+    //     return false;
     // set cloud_out frame_id
-    cloud_out.header.frame_id = cloud_in1.header.frame_id;
+    cloud_out.header.frame_id = FIXED_FRAME;
     // merge the clouds one by one
     merge_point32_vector(cloud_in1.points, cloud_out.points);
     merge_point32_vector(cloud_in2.points, cloud_out.points);
@@ -279,8 +205,6 @@ int main(int argc, char **argv)
 
   //Create an object of class SubscribeAndPublish that will take care of everything
   SubscribeAndPublish SAPObject;
-
-  ros::spin();
 
   return 0;
 }
